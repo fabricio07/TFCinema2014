a1e574152130e644d0deaabe18bd8e1a68d9e6c1
Fabrício <Fabrcio@Beretta>
2014-06-29 14:25:17 -0300

    TFAlproII-FilasCinemas

---
+++ src/sepermercado/SimuladorSupermercado.java
@@ -0,0 +1,14 @@
+package sepermercado;
+
+/*
+ * Programa principal da simulacao
+ */
+public class SimuladorSupermercado
+{
+    public static void main(String[] args)
+    {
+        Simulacao sim = new Simulacao(true);
+        sim.simular();
+        sim.imprimirResultados();
+    }
+}
